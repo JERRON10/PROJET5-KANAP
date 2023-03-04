14449bffdcf718e2fcbc477dd5757ad678d4867b
JRM <jeromeperron87@gmail.com>
2023-03-04 19:02:22 +0100

    début étape 11

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -1,3 +1,6 @@
+// ********************************** LE PANIER ***********************************
+
+//Les variables
 let dataproduct;
 let total_quantity = 0;
 let total_price = 0;
@@ -10,28 +13,24 @@ async function init() {
 
     // 1 - Récupèrer les données du LocalStorage
     chargeLS;
-    console.log("avant=>", total_quantity)
     total_quantity = 0;
     total_price = 0;
-
-
     // 2 - On créer les élements et on calcul le total
     for (let i = 0; i < chargeLS.length; i++) {
-        let reponsePageProduct = await fetch(`http://localhost:3000/api/products/${chargeLS[i].id}`);
-        dataproduct = await reponsePageProduct.json();
 
+        let reponsePageProduct = await useFetch(chargeLS[i].id);
+        dataproduct = await reponsePageProduct.json();
         createProductHtmlInCartAndMangeEvents(i, dataproduct)
-        console.log("avant=>", total_quantity)
         total_price += chargeLS[i].quantity * dataproduct.price;
         total_quantity += chargeLS[i].quantity;
-        console.log("après quantités => ", total_quantity);
-        console.log("quantités chargels => ", chargeLS[i].quantity);
-    
     }
-
     // 3 - Maintenant qu'on a les totaux on les affiche
-    createCartTotalPrice(total_price);  
+    createCartTotalPrice(total_price);
     createCartTotalQuantity(total_quantity);
+}
+
+async function useFetch(path) {
+    return await fetch(`http://localhost:3000/api/products/${path}`);
 }
 
 // Récupére le localStorage
@@ -40,8 +39,8 @@ async function chargeLocalstorage() {
 }
 
 // Sauvegarde dans le localStorage
-async function saveLocalstorage() {
-    localStorage.setItem("product", JSON.stringify(chargeLS));
+async function saveLocalstorage(clé, valeur) {
+    localStorage.setItem(clé, JSON.stringify(valeur));
 }
 
 // récupérer la quantité, la couleur et l'id des inputs
@@ -54,20 +53,18 @@ function manageProductQuantityChanged(quantityInput) {
         collectQuantity = parseInt(quantityInput.value);
         collectId = quantityInput.closest(".cart__item").dataset.id;
         collectColor = quantityInput.closest(".cart__item").dataset.color;
-        console.log("récupère les info selectionnées=>", collectQuantity, collectId, collectColor)
+
         let search = chargeLS.findIndex(product => product.id === collectId &&
             product.color === collectColor && product.quantity !== collectQuantity);
-        if (search !== -1)
+
+        if (search !== -1) {
             chargeLS[search].quantity = collectQuantity;
-            console.log("quantités 2test => ", total_quantity);
-
-        saveLocalstorage();
-        deleteWithHtml("#cart__items");
-        deleteWithHtml("#totalQuantity");
-        deleteWithHtml("#totalPrice");
-        init();
-        console.log("quantités 3test => ", total_quantity);
-
+            saveLocalstorage("product", chargeLS);
+            deleteWithHtml("#cart__items");
+            deleteWithHtml("#totalQuantity");
+            deleteWithHtml("#totalPrice");
+            init();
+        }
     });
 }
 
@@ -81,8 +78,7 @@ function manageProductDeleted(settingsDeleteItem) {
         for (let i = chargeLS.length - 1; i >= 0; i--) {
             if (chargeLS[i].id === collectId && chargeLS[i].color === collectColor) {
                 chargeLS.splice(i, 1);
-
-                saveLocalstorage();
+                saveLocalstorage("product", chargeLS);
                 deleteWithHtml("#cart__items");
                 deleteWithHtml("#totalQuantity");
                 deleteWithHtml("#totalPrice");
@@ -176,7 +172,6 @@ function createProductHtmlInCartAndMangeEvents(index, product_to_create) {
     settingsDelete.appendChild(settingsDeleteItem);
 
     // 2 - Gérer les events
-    // Maintenant que j'ai créé mon element, j'aimerais ecouter les evenements change et delete
     manageProductQuantityChanged(quantityInput);
     manageProductDeleted(settingsDeleteItem);
 }
@@ -186,3 +181,158 @@ async function deleteWithHtml(tag) {
     document.querySelector(tag).innerHTML = "";
 }
 
+// ********************************* FORMULAIRE ************************************
+
+// RegExp firstName
+let firstName = document.querySelector("#firstName");
+firstName.addEventListener("change", function () {
+    validFirstName(this);
+})
+
+// RegExp lastName
+let lastName = document.querySelector("#lastName");
+lastName.addEventListener("change", function () {
+    validLastName(this);
+})
+
+// RegExp address
+let address = document.querySelector("#address");
+address.addEventListener("change", function () {
+    validAddress(this);
+})
+
+// RegExp city
+let city = document.querySelector("#city");
+city.addEventListener("change", function () {
+    validCity(this);
+})
+
+// RegExp email
+let email = document.querySelector("#email");
+email.addEventListener("change", function () {
+    validEmail(this);
+})
+
+// Validation du formulaire
+let submitForm = document.querySelector("#order");
+submitForm.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    // Mon objet contact
+    let contact = {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        address: address.value,
+        city: city.value,
+        email: email.value
+    };
+
+    // Mon tableau product_id
+    let products = [];
+    for (let i = 0; i < chargeLS.length; i++) {
+        products.push(chargeLS[i].id);
+    }
+
+    // Mon objet à envoyer à l'API (contact, products)
+    let chargeUtile = {
+        contact,
+        products
+    }
+
+    console.log("contact=>", contact);
+    console.log("produit_id=>", products);
+    console.log("chargeUtile=>", chargeUtile);
+
+    if ((validFirstName(firstName) && validLastName(lastName)
+        && validAddress(address) && validCity(city) && validEmail(email)) == true) {
+
+        // saveLocalstorage("client", formValues);
+
+        fetch("http://localhost:3000/api/products/order", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(chargeUtile)
+        });
+    } else {
+        console.log("false");
+        alert("Formulaire non valide");
+    }
+});
+
+let validFirstName = function (inputselect) {
+    let FirstNameRegExp = new RegExp(
+        "^[A-Za-z\é\è\ê\-ôï]{2,25}$", "g"
+    );
+    let testFirstName = FirstNameRegExp.test(inputselect.value);
+    let firstNameErrorMessage = document.querySelector("#firstNameErrorMsg");
+    if (testFirstName == false) {
+        firstNameErrorMessage = document.querySelector("#firstNameErrorMsg");
+        firstNameErrorMessage.innerText = "Veuillez entrer un prénom valide";
+        return false;
+    } else {
+        firstNameErrorMessage.innerHTML = "";
+        return true;
+    }
+}
+
+let validLastName = function (inputselect) {
+    let lastNameRegExp = new RegExp(
+        "^[A-Za-z\é\è\ê\-ôï]{2,25}$", "g"
+    );
+    let testName = lastNameRegExp.test(inputselect.value);
+    let lastNameErrorMessage = document.querySelector("#lastNameErrorMsg");
+    if (testName == false) {
+        lastNameErrorMessage = document.querySelector("#lastNameErrorMsg");
+        lastNameErrorMessage.innerText = "Veuillez entrer un nom valide";
+        return false;
+    } else {
+        lastNameErrorMessage.innerHTML = "";
+        return true;
+    }
+}
+
+let validAddress = function (inputselect) {
+    let addressRegExp = new RegExp(
+        "^([0-9a-zA-Z\-\w]{0,9}) ([a-zA-Z\ \-éèêï\w]*)$", "g"
+    );
+    let testAddress = addressRegExp.test(inputselect.value);
+    let addressErrorMessage = document.querySelector("#addressErrorMsg");
+    if (testAddress == false) {
+        addressErrorMessage = document.querySelector("#addressErrorMsg");
+        addressErrorMessage.innerText = "Veuillez entrer une adresse valide";
+        return false;
+    } else {
+        addressErrorMessage.innerHTML = "";
+        return true;
+    }
+}
+
+let validCity = function (inputselect) {
+    let cityRegExp = new RegExp(
+        "^([A-Za-z\é\è\ê\-ôï\ ]){2,25}$", "g");
+    let testCity = cityRegExp.test(inputselect.value);
+    let cityErrorMessage = document.querySelector("#cityErrorMsg");
+    if (testCity == false) {
+        cityErrorMessage = document.querySelector("#cityErrorMsg");
+        cityErrorMessage.innerText = "Veuillez entrer une ville valide";
+        return false;
+    } else {
+        cityErrorMessage.innerHTML = "";
+        return true;
+    }
+}
+
+let validEmail = function (inputselect) {
+    let emailRegExp = new RegExp(
+        "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,4}$", "g");
+    let testEmail = emailRegExp.test(inputselect.value);
+    let emailErrorMessage = document.querySelector("#emailErrorMsg");
+    if (testEmail == false) {
+        emailErrorMessage = document.querySelector("#emailErrorMsg");
+        emailErrorMessage.innerText = "Veuillez entrer un mail valide";
+        return false;
+    } else {
+        emailErrorMessage.innerHTML = "";
+        return true;
+    }
+}
